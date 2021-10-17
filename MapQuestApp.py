dbfb060e46f21b64e0eaecd4e12389c394e910b0
diannepardo20 <92463171+diannepardo20@users.noreply.github.com>
2021-10-17 21:10:26 +0800

    Update MapQuestApp.py

--- MapQuestApp.py
+++ MapQuestApp.py
@@ -10,33 +10,39 @@ print(Back.WHITE + "\n\n---------------------------------------- MAPQUEST DIRECT
 print(Style.RESET_ALL)#TO RESET THE STYLE ADDED AND NOT BE INHERITED BY THE NEXT LINE
 
 while True:
-    
     answer = input("\nChoose on what you want to do \n\nA.Start MAPQUEST Application\nB.How to use the application \nC.Quit\n\nAnswer: ") #TO OUTPUT OPTION VALUES
+
     if answer == "A" or answer == "a": #OPTION VALUE
-        orig = input("Starting Location: ")
+        orig = input("\n\nStarting Location: ")
         if orig == "quit" or orig == "q":
             break
-        dest = input("Destination: ")
+        dest = input("\nDestination: ")
         if dest == "quit" or dest == "q":
             break
-        url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest}) 
+        url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest}) #TO GENERATE MAPQUEST LINK
         json_data = requests.get(url).json()
-        print("URL: " + (url))
+        print("\nURL: " + (url)) #TO OUTPUT MAPQUEST LINK
         json_data = requests.get(url).json()
-        json_status = json_data["info"]["statuscode"]
 
+        json_status = json_data["info"]["statuscode"]
         if json_status == 0:
-            print("API Status: " + str(json_status) + " = A successful route call.\n")
-            print("=============================================")
-            print("Directions from " + (orig) + " to " + (dest))
+            print("\nAPI Status: " + str(json_status) + " = A successful route call.")
+
+            print(Fore.BLACK + Style.DIM)#FOR ADDING OF FONT COLOR AND THE OPACITY OF THE FONT
+            print(Back.WHITE + "\n\n--------------------------------------------------- DIRECTIONS --------------------------------------------------")#FOR ADDING BACKGROUND HIGHLIGHT OF THE TEXT
+            print(Style.RESET_ALL)#TO RESET THE STYLE ADDED AND NOT BE INHERITED BY THE NEXT LINE
+
+
+            print("\nRequesting Directions from " + (orig) + " to " + (dest))
             print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
             print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
             print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
-            print("=============================================")
+            print("\n--DIRECTIONS--")
             for each in json_data["route"]["legs"][0]["maneuvers"]:
                 print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
-            print("=============================================\n")
 
+        // add mo dito gela after 
+        
         elif json_status == 402:
             print("********************************************")
             print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
